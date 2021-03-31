1cd90befd27e1b3d7f7d0d84317029eb38be9c3e
lelouchB <ashutoshs.ic18@nsut.ac.in>
2021-03-31 10:34:52 +0530

    Adding API Version

--- src/client.js
+++ src/client.js
@@ -4,4 +4,5 @@ export default sanityClient({
   projectId: process.env.VUE_APP_SANITY_PROJECT_ID, // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
+  apiVersion: "2021-03-25"
 });
